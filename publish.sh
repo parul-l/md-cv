62d0ebac0f24549aab06c3587f6df6c5dbadefb6
parul-l <parul.laul@gmail.com>
2022-08-14 20:44:44 -0400

    include .md files in publish

--- publish.sh
+++ publish.sh
@@ -4,12 +4,16 @@
 jekyll build
 
 # path on your system to your github pages repo checkout
-SITE_DIR=../parul-l.github.io/resume/
+SITE_DIR=../parul-l.github.io
 
 
-# # remove old files
-rm -R ${SITE_DIR}*
+# # remove old resume files
+rm -R ${SITE_DIR}/resume/*
 
 # # re-add new
-cp _site/index.html ${SITE_DIR}.
-cp -R _site/media ${SITE_DIR}.
+cp _site/index.html ${SITE_DIR}/resume/.
+cp -R _site/media ${SITE_DIR}/resume/.
+
+## copy layout and resume.md
+cp _layouts/cv.html ${SITE_DIR}/_layouts/cv_html
+cp index.md ${SITE_DIR}/resume.md
